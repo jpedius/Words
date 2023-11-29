890f4a18d91cdbca031d3b1cddb7c37a134853fb
JP McDowell <jp.mcdowell@gmail.com>
2023-11-28 20:37:31 -0800

    Commit me

--- source/main.lua
+++ source/main.lua
@@ -4383,7 +4383,7 @@ local words = {
 	"transportation"
 };
 
-function my_shuffle(a)
+function MyShuffle(a)
     local z = {}
     for i = #a, 1, -1 do
         local j = math.random(i)
@@ -4393,7 +4393,36 @@ function my_shuffle(a)
     return z
 end
 
-words = my_shuffle(words)
+words = MyShuffle(words)
+
+local scrambled = {}
+
+function MyScrambled(w, p)
+    local s = {}
+    for key, value in pairs(w) do
+        if #value <= p then
+            local a = ""
+            for i = 1, #value do
+                a = a .. "-"
+            end
+            table.insert(s, a)
+        else
+            local b = {}
+            for i = 1, #value do
+                b[i] = i
+            end
+            b = MyShuffle(b)
+            local c = value
+            for i = 1, p do
+                c = string.sub(c, 0, b[i]-1) .. "-" .. string.sub(c, b[i]+1)
+            end
+            table.insert(s, c)
+        end
+    end
+    return s
+end
+
+scrambled = MyScrambled(words, 3)
 
 local width  = 200
 local height = 120
@@ -4407,9 +4436,14 @@ function playdate.update()
     local w,h = gfx.getTextSize(words[pos])
 
     local mfw = math.floor((width / 2) - (w / 2))
-    local mfh = math.floor((height / 2) - (h / 2))
+    local mfh = math.floor((height / 4) - (h / 4))
 
     gfx.drawText(words[pos], mfw, mfh)
+
+    mfw = math.floor((width / 2) - (w / 2))
+    mfh = math.floor(((height / 4) - (h / 2)) + 60)
+
+    gfx.drawText(scrambled[pos], mfw, mfh)
 
 end
 
